0490a64e09b884eef533d439b12159de44dd8733
Jonathan Bizier <jonathan.bizier@osedea.com>
2019-07-02 17:40:50 -0400

    More fixing

--- step.sh
+++ step.sh
@@ -2,7 +2,7 @@
 set -ex
 
 brew cask install docker-toolbox
-docker-machine create --driver "virtualbox" default --virtualbox-no-vtx-check
+docker-machine create -d virtualbox --virtualbox-no-vtx-check default 
 
 # Need to be run in each script requiring Docker
 eval $(docker-machine env default)
