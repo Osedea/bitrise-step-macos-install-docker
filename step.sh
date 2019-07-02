d6f0f47879374cfaaa445f66394c80cf9286dc9e
Jonathan Bizier <jonathan.bizier@osedea.com>
2019-07-02 17:32:40 -0400

    Fixing bitrise builds

--- step.sh
+++ step.sh
@@ -2,7 +2,7 @@
 set -ex
 
 brew cask install docker-toolbox
-docker-machine create --driver "virtualbox" default
+docker-machine create --driver "virtualbox" default --virtualbox-no-vtx-check
 
 # Need to be run in each script requiring Docker
 eval $(docker-machine env default)
